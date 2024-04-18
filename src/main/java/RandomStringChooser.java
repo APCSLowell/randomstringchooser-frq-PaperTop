e455bc87f128d14234b2459814e6391025347cf3
PaperTop <135560395+PaperTop@users.noreply.github.com>
2024-04-17 17:57:50 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -5,15 +5,14 @@ public class RandomStringChooser
   private ArrayList <String> words;
   public RandomStringChooser(String[] x){
     words = new ArrayList<String>();
-    for (int i  = 0; i < x.length; i++){
+    for (int i  = 0; i < x.length; i++)
       words.add(x[i]);
-    }
   }
   public String getNext(){
-    if (words.size() == 0){
+    if (words.size() == 0)
       return "NONE";
-    }
-    int index = Math.random() * words.size();
+    
+    int index = (int) Math.random() * words.size();
     String temp = words.get(index);
     words.remove(index);
     return temp;
